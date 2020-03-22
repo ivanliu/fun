ad986f8ffdf3d5e06e5ac31c77c1442f6e48042e
Kai Liu <ioscas@gmail.com>
2020-03-22 23:09:51 +0800

    Update merge_uuid.py

--- python/merge_uuid.py
+++ python/merge_uuid.py
@@ -11,7 +11,7 @@ def load_uuid(_file):
     
     uuid = set()
     for line in fin:
-        _id = line.strip()
+        _id = line.split(',')[0].strip()
         uuid.add(_id)
     fin.close()
     
@@ -31,21 +31,13 @@ def main():
     '''
     _input1 = ''
     _input2 = ''
-    _input3 = ''
-    _output1 = ''
-    _output2 = ''
+    _output = ''
 
-    all_uuid = load_uuid(_input1)  # the file contains all uuid
-    acct_uuid = load_uuid(_input2) # the file contains all users who open account
-    cash_uuid = load_uuid(_input3) # the file contains all users who deposit cash
-    
-    # find acct users and cash users
-    interested_acct_uuid = all_uuid.intersection(acct_uuid)
-    interested_cash_uuid = all_uuid.intersection(cash_uuid)
-    
+    omnibus_uuid = load_uuid(_input1)  # the file contains omni uuid
+    global_uuid = load_uuid(_input2) # the file contains global uuid
+   
     # dump uuids to files
-    dump_uuid(interested_acct_uuid, _output1)
-    dump_uuid(interested_cash_uuid, _output2)
+    dump_uuid(omnibus_uuid.union(global_uuid), _output)
     
 
 if __name__ == '__main__':
